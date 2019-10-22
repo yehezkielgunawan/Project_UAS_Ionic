5ca03f1ab617dc9f059cf1e11e534eac1ec8e817
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2019-10-22 20:19:35 +0700

    Added Ionic project

---
+++ Elearning_Gamification/src/app/app.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss']
+})
+export class AppComponent {
+
+  public appPages=[
+    {
+      title: 'Home',
+      url: '/home',
+      icon: 'home'
+    },
+    {
+      title: 'My Course',
+      url: '/my_course',
+      icon: 'book'
+    },
+    {
+      title: 'LOGOUT',
+      url: '/login',
+      icon: 'exit'
+    }
+  ];
+
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+}
